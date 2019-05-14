e5836819ed53c776354abc1ccf05cac21676b9a7
danilomacb <danilo.bakun@gmail.com>
2019-05-14 14:18:34 -0300

    initial commit

---
+++ functions.test.js
@@ -0,0 +1,77 @@
+const functions = require("./functions");
+
+// beforeEach(() => initDatabase());
+// afterEach(() => closeDatabase());
+
+// beforeAll(() => initDatabase());
+// afterAll(() => closeDatabase());
+
+// const initDatabase = () => console.log("Database Initialized...");
+// const closeDatabase = () => console.log("Database Closed...");
+
+const nameCheck = () => console.log("Checking Name...");
+
+describe("Checking Names", () => {
+  beforeEach(() => nameCheck());
+
+  test("User is Jeff", () => {
+    const user = "Jeff";
+    expect(user).toBe("Jeff");
+  });
+
+  test("User is Karen", () => {
+    const user = "Karen";
+    expect(user).toBe("Karen");
+  });
+});
+
+test("Adds 2 + 2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+
+test("Adds 2 + 2 to NOT equal 5", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+test("Should be falsy", () => {
+  expect(functions.checkValue(undefined)).toBeFalsy();
+});
+
+test("User Should be Danilo Bakun object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Danilo",
+    lastName: "Bakun"
+  });
+});
+
+test("Should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 800;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/i);
+});
+
+test("Admin should be in usernames", () => {
+  usernames = ["jhon", "karen", "admin"];
+  expect(usernames).toContain("admin");
+});
+
+// test("User fetched name should be Leanne Graham", () => {
+//   expect.assertions(1);
+//   return functions.fetchUser().then(data => {
+//     expect(data.name).toEqual("Leanne Graham1");
+//   });
+// });
+
+test("User fetched name should be Leanne Graham", async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual("Leanne Graham");
+});
